4ca67390075e50a911e1bf558b11bf3995746967
Theodore Boyd <tb208@doc.ic.ac.uk>
2011-08-27 01:33:44 +0100

    Initial commit of GAE project

---
+++ urls.py
@@ -0,0 +1,16 @@
+import views
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+application = webapp.WSGIApplication([
+  ('/', views.Home),
+  ('/guestbook', views.Guestbook)
+], debug=True)
+
+
+def main():
+  run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
