069d9c5cae4fd64df06c006322e426915b154bfa
Theodore Boyd <tb208@doc.ic.ac.uk>
2011-08-28 14:04:13 +0100

    All working with static files

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 # Copyright 2011 Theodore Boyd
+import settings
 import views
 
 from django.conf.urls.defaults import *
@@ -6,9 +7,12 @@ from django.conf.urls.defaults import *
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 urlpatterns = patterns('',
+    # Framework
     (r'^_ah/warmup$', 'djangoappengine.views.warmup'),
     (r'^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html'}),
+
+    # Pages
     (r'^guestbook/sign/$', views.GuestbookSign),
     (r'^guestbook/view/$', views.GuestbookView),
     (r'^guestbook/delete/(?P<key>[a-zA-Z0-9_.-]+)/$', views.GuestbookDeleteEntry),
